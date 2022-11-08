893fd91a31a284e21af003bca0ecdf0797e50b21
Muzaffar <muzaffaroffical7@gmail.com>
2022-11-08 10:52:58 +0300

    Polindrom.py bo'sh faylini qo'shdim

--- polindrom.py
+++ polindrom.py
@@ -1,5 +1,7 @@
 st=input("satrni kiriting: ")
-if st==[::-1]:
+if st==st[::-1]:
     print("polindrom")
 else:
-    print("polindrom emas")    
+    print("polindrom emas")  
+def func(s):
+    return s[::-1]      
